73010f212f1cd39df9718840eb2432222fb8d1a7
codeswithankit <ank8804465648@gmail.com>
2020-07-10 09:47:34 +0530

    improving the http hook and use it to get users on UI and more

--- client/src/user/pages/Auth.js
+++ client/src/user/pages/Auth.js
@@ -1,88 +1,75 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from "react";
 
-import Card from '../../shared/components/UIElements/Card'
-import Button from '../../shared/components/FormElements/Button'
-import Input from '../../shared/components/FormElements/Input'
-import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner'
-import ErrorModal from '../../shared/components/UIElements/ErrorModal'
+import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/components/FormElements/Button";
+import Input from "../../shared/components/FormElements/Input";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE,
-} from '../../shared/util/validators'
-import { useForm } from '../../shared/hooks/form-hook'
-import { AuthContext } from '../../shared/context/auth-context'
-import './Auth.css'
+} from "../../shared/util/validators";
+import { useForm } from "../../shared/hooks/form-hook";
+import { AuthContext } from "../../shared/context/auth-context";
+import "./Auth.css";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
 const Auth = () => {
-  const auth = useContext(AuthContext)
-  const [isLoginMode, setIsLoginMode] = useState(true)
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState()
+  const auth = useContext(AuthContext);
+  const [isLoginMode, setIsLoginMode] = useState(true);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
   const [formState, inputHandler, setFormData] = useForm({
     email: {
-      value: '',
+      value: "",
       isValid: false,
     },
     password: {
-      value: '',
+      value: "",
       isValid: false,
     },
-  })
+  });
 
   const authSubmitHandler = async (event) => {
-    event.preventDefault()
-    setIsLoading(true)
-
+    event.preventDefault();
     if (isLoginMode) {
       try {
-        const response = await fetch('http://localhost:5000/api/users/login', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
+        await sendRequest(
+          "http://localhost:5000/api/users/login",
+          "POST",
+          JSON.stringify({
             email: formState.inputs.email.value,
             password: formState.inputs.password.value,
           }),
-        })
+          {
+            "Content-Type": "application/json",
+          },
+        );
 
-        const responseData = await response.json()
-        if (!response.ok) {
-          throw new Error(responseData.message)
-        }
-        setIsLoading(false)
-        auth.login()
-      } catch (err) {
-        setIsLoading(false)
-        setError(err.message || 'Something went wrong, please try again.')
+        auth.login();
+      } catch (error) {
       }
     } else {
       try {
-        const response = await fetch('http://localhost:5000/api/users/signup', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
+        await sendRequest(
+          "http://localhost:5000/api/users/signup",
+          "POST",
+          JSON.stringify({
             name: formState.inputs.name.value,
             email: formState.inputs.email.value,
             password: formState.inputs.password.value,
           }),
-        })
+          {
+            "Content-Type": "application/json",
+          },
+        );
 
-        const responseData = await response.json()
-        if (!response.ok) {
-          throw new Error(responseData.message)
-        }
-        setIsLoading(false)
-        auth.login()
+        auth.login();
       } catch (err) {
-        setIsLoading(false)
-        setError(err.message || 'Something went wrong, please try again.')
       }
     }
-  }
+  };
 
   const switchModeHandler = () => {
     if (!isLoginMode) {
@@ -91,25 +78,26 @@ const Auth = () => {
           ...formState.inputs,
           name: undefined,
         },
-        formState.inputs.email.isValid && formState.inputs.password.isValid
-      )
+        formState.inputs.email.isValid && formState.inputs.password.isValid,
+      );
     } else {
       setFormData(
         {
           ...formState.inputs,
           name: {
-            value: '',
+            value: "",
             isValid: false,
           },
         },
-        false
-      )
+        false,
+      );
     }
-    setIsLoginMode((prevMode) => !prevMode)
-  }
+    setIsLoginMode((prevMode) => !prevMode);
+  };
+
   const errorHandler = () => {
-    setError(null)
-  }
+    clearError();
+  };
   return (
     <>
       <ErrorModal error={error} onClear={errorHandler} />
@@ -148,14 +136,14 @@ const Auth = () => {
             onInput={inputHandler}
           />
           <Button type="submit" disabled={!formState.isValid}>
-            {isLoginMode ? 'LOGIN' : 'SIGNUP'}
+            {isLoginMode ? "LOGIN" : "SIGNUP"}
           </Button>
         </form>
         <Button inverse onClick={switchModeHandler}>
-          SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
+          SWITCH TO {isLoginMode ? "SIGNUP" : "LOGIN"}
         </Button>
       </Card>
     </>
-  )
-}
-export default Auth
+  );
+};
+export default Auth;
--- client/src/user/pages/Users.js
+++ client/src/user/pages/Users.js
@@ -1,36 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import UsersList from '../components/UsersList'
-import ErrorModal from '../../shared/components/UIElements/ErrorModal'
-import loadingSpinner from '../../shared/components/UIElements/LoadingSpinner'
+import React, { useEffect, useState } from "react";
+import UsersList from "../components/UsersList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import loadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
 const Users = () => {
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState()
-  const [loadedUsers, setLoadedUsers] = useState()
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedUsers, setLoadedUsers] = useState();
 
   useEffect(() => {
-    const sendRequest = async () => {
+    const fetchUsers = async () => {
       try {
-        setIsLoading(true)
-        const response = await fetch('http://localhost:5000/api/users')
-        const responseData = await response.json()
-        if (!response.ok) {
-          throw new Error(responseData.message)
-        }
+        const responseData = await sendRequest(
+          "http://localhost:5000/api/users",
+        );
 
-        setLoadedUsers(responseData.users)
+        setLoadedUsers(responseData.users);
       } catch (error) {
-        setIsLoading(false)
-        setError(error.message)
       }
-      setIsLoading(false)
-    }
-    sendRequest()
-  }, [])
-
-  const errorHandler = () => {
-    setError(null)
-  }
+    };
+    fetchUsers();
+  }, [sendRequest]);
 
   // const USERS = [
   //   {
@@ -44,15 +34,16 @@ const Users = () => {
 
   return (
     <>
-      <ErrorModal error={error} onClear={errorHandler} />
+      <ErrorModal error={error} onClear={clearError} />
       {isLoading && (
         <div className="center">
           <loadingSpinner />
         </div>
       )}
-      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}{' '}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+      {" "}
     </>
-  )
-}
+  );
+};
 
-export default Users
+export default Users;
